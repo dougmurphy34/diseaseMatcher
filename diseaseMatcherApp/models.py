627f2d6b37d28ce28279c47aa0d90d080d9d4b64
dougmurphy34 <dougmurphy34@gmail.com>
2014-08-18 13:38:29 -0700

    Removed South due to incompatibility with MariaDB

--- diseaseMatcherApp/models.py
+++ diseaseMatcherApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import datetime
+from django.utils import timezone
 
 # Create your models here.
 
@@ -16,7 +17,11 @@ class Abstract(models.Model):
 #users
 class annotator(models.Model):
     username = models.TextField(max_length=25)
-    last_entry_date = models.DateTimeField()
+    last_entry_date = models.DateTimeField(default=timezone.now())
 
     def __unicode__(self):
         return self.username
+
+class unused_temp_class(models.Model):
+    string_field = models.TextField(max_length=10)
+    number_field = models.IntegerField(default=0)
