a48ad54b230f18578981ed18367c927e4110f49a
dougmurphy34 <dougmurphy34@gmail.com>
2014-08-18 10:12:30 -0700

    Tweak objective

--- diseaseMatcherApp/models.py
+++ diseaseMatcherApp/models.py
@@ -6,7 +6,8 @@ import datetime
 
 class Abstract(models.Model):
     abstract_id = models.IntegerField()
-    title = models.TextField(max_length=5000)
+    title = models.TextField(max_length=500)
+    abstract_text = models.TextField(max_length = 5000)
     pub_date = models.DateTimeField()
 
     def __unicode__(self):
