88edd3a4a20b763af6dfd0d2427f25a0925fe5d4
dougmurphy34 <dougmurphy34@gmail.com>
2014-08-18 11:45:10 -0700

    Added annotator (additional GitHub test)

--- diseaseMatcherApp/models.py
+++ diseaseMatcherApp/models.py
@@ -3,11 +3,16 @@ import datetime
 
 # Create your models here.
 
-
+#Raw data to be classified
 class Abstract(models.Model):
     abstract_id = models.IntegerField()
-    title = models.TextField(max_length=5000)
+    title = models.TextField(max_length=500)
+    abstract_text = models.TextField(max_length=5000)
     pub_date = models.DateTimeField()
 
     def __unicode__(self):
         return self.title
+
+#users who
+class annotator(models.Model):
+    username = models.TextField(max_length=25)
