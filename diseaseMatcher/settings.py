924e4b4ac6cdcd42e97e1cc68a1de0cf5ac49e7c
dougmurphy34 <dougmurphy34@gmail.com>
2014-08-18 15:13:46 -0700

    Early schema changes

--- diseaseMatcher/settings.py
+++ diseaseMatcher/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'diseaseMatcherApp'
+    'diseaseMatcherApp',
 )
 
 MIDDLEWARE_CLASSES = (
