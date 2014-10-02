50c14c1c401963893aaa2b50c7fcab723d0ab841
dougmurphy34 <dougmurphy34@gmail.com>
2014-10-02 15:45:58 -0700

    tweak static directory path so it will work

--- diseaseMatcher/settings.py
+++ diseaseMatcher/settings.py
@@ -94,8 +94,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#this is needed for sstatic template tag.  TODO: REMOVE SLASHES FOR COMPATIBILITY WITH dj-static and Heroku?
-STATIC_ROOT = '/diseaseMatcherApp/'
+#this is needed for sstatic template tag.  TODO: TRYING: REMOVE lead/trail SLASHES FOR COMPATIBILITY WITH dj-static and Heroku?
+STATIC_ROOT = 'diseaseMatcherApp'
 
 #I think this is redundant with BASE_DIR, but for now I'm following directions exactly from:
 #https://devcenter.heroku.com/articles/django-assets
